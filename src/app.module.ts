48ed4d96603888366b53fd6b3d6799ac9526ef97
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2024-09-01 11:20:01 -0300

    feat: changing mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NotificationsGateway } from './notifications/notifications.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatModule } from './chat/chat.module';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:root@localhost:27017/chat'),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     ChatModule,
     NotificationsModule,
   ],
